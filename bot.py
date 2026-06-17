8b18168de926cd2980dfbfadc81af7a95d10ca68
omax70187-commits <omax70187@gmail.com>
2026-06-17 18:52:51 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -210,7 +210,10 @@ async def monitor_loop():
                 client = TikTokLiveClient(unique_id=user)
                 
                 print(f"Cliente TikTok creado: {user}")
-                print(f"Preparando comprobación: {user}") 
+                print(f"Preparando comprobación: {user}")
+                
+                if user not in recording_users:
+                    await start_recording(user)
 
             except Exception as e:
                 print(f"Error TikTok {user}: {e}")
