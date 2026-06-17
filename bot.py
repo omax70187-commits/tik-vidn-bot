920786523095310753a7e9dfaea55bc582bb9c4c
omax70187-commits <omax70187@gmail.com>
2026-06-17 00:23:11 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,11 +5,27 @@ from telegram.ext import Application, CommandHandler, ContextTypes
 
 print("=== INICIO ===")
 print("BOT_TOKEN =", os.getenv("BOT_TOKEN"))
+watched_users = []
 
 TOKEN = os.getenv("BOT_TOKEN")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Bot TikTok Recorder activo.")
+async def monitor(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if not context.args:
+        await update.message.reply_text(
+            "Uso: /monitor usuario"
+        )
+        return
+
+    username = context.args[0]
+
+    if username not in watched_users:
+        watched_users.append(username)
+
+    await update.message.reply_text(
+        f"Ahora vigilando: {username}"
+    )
 
 def main():
     if not TOKEN:
@@ -18,6 +34,7 @@ def main():
     app = Application.builder().token(TOKEN).build()
 
     app.add_handler(CommandHandler("start", start))
+    app.add_handler(CommandHandler("monitor", monitor))
 
     print("Bot iniciado...")
     app.run_polling()
