ad88345d887525aa762acd8a0064f80bbb4cd373
omax70187-commits <omax70187@gmail.com>
2026-06-16 23:37:14 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,15 +3,18 @@ import os
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
 
+print("=== INICIO ===")
+print("BOT_TOKEN =", os.getenv("BOT_TOKEN"))
+
 TOKEN = os.getenv("BOT_TOKEN")
-print("TOKEN CARGADO:", TOKEN)
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text(
-        "Bot TikTok Recorder activo."
-    )
+    await update.message.reply_text("Bot TikTok Recorder activo.")
 
 def main():
+    if not TOKEN:
+        raise Exception("BOT_TOKEN no fue encontrado en Railway")
+
     app = Application.builder().token(TOKEN).build()
 
     app.add_handler(CommandHandler("start", start))
